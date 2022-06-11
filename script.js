5c5fdc6f4adcd7edc64b226825d38ee004460ad9
kurniadikevin <kurniadikevin@gmail.com>
2022-06-11 19:11:10 +0700

    added some styling

--- script.js
+++ script.js
@@ -10,24 +10,19 @@ function book(title,author,pages,status) {
     return `${title}, ${author}, ${pages}, ${status}`;
   }
 }
-
 function addBookToLibrary(book) {
   // do stuff here
   myLibrary.push( (book.info()).split(', ') );
  
 }
 
-// add book function to my library on addBookTo
-
 //testing
 /*
 const hobbit = new book('The Hobbit','J.R.R. Tolkien','295','not read yet');
 const harryPot = new book('Harry Potter','J.K Rowling','764','read');
-
 console.log(hobbit.info());
 addBookToLibrary(hobbit);
 addBookToLibrary(harryPot);
-
 */
 
 //DOM
@@ -46,8 +41,7 @@ for(let i=0; i< myLibrary.length; i++){
 let bookStatus = document.querySelector('#status');
 bookStatus.addEventListener('change',function(ev){
   bookStatus.value = ev.target.value;
- 
-});
+ });
 
 //dom for ad button
 let addButton = document.querySelector('#add-book');
@@ -68,7 +62,6 @@ addButton.addEventListener('click',function(){
       // make display on main data
       for(let j=0; j<myLibrary[myLibrary.length-1].length; j++){
             if(j === 3){
-              
               let data = document.createElement('div');
               data.classList.add('data');
               bookGroup.appendChild(data);
@@ -86,8 +79,6 @@ addButton.addEventListener('click',function(){
                 myLibrary[myLibrary.length-1][j] = ev.target.value;
                 console.log(myLibrary);
               })
-              
-
               
               //delete button
               let deleteBtn= document.createElement('button');
